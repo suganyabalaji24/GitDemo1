4c07d7a825253f8e37a7668019d9e2002f0bd95a
suganyabalaji24 <suganya_indian@yahoo.co.uk>
2022-10-09 23:17:18 +0530

    Git demo1

---
+++ src/main/java/Git_Demo1/Push_In_Terminal/App.java
@@ -0,0 +1,13 @@
+package Git_Demo1.Push_In_Terminal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
